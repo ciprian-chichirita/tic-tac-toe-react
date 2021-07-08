da0b932d250e50dbc3497fb15ab07395368c7a9c
Ciprian Chichirita <ciprian.chichirita@amag.com>
2021-07-08 14:25:01 +0300

    move build to docs folder

---
+++ docs/precache-manifest.b37c29100794a76095d2010e9ae73e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508fcf60a2f8e9659f6a7d61a16a30de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd4027cc47c6debdfe02",
+    "url": "/static/css/main.8e40ed4c.chunk.css"
+  },
+  {
+    "revision": "b5cf74df6b3d3f39000c",
+    "url": "/static/js/2.05013cac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05013cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4027cc47c6debdfe02",
+    "url": "/static/js/main.25e1c394.chunk.js"
+  },
+  {
+    "revision": "46e1ce656ac57e3635d1",
+    "url": "/static/js/runtime-main.876764e1.js"
+  }
+]);
